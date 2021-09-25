c347b4b89b7d2a935a55ba5a3d52fba7b49f538d
Fernando Goncalves <fernando.hmg@gmail.com>
2021-09-25 16:56:38 -0300

    Creating Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
+import { Flex, Stack } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import React from "react";
+import { Input } from "../components/Form/Input";
 
-const Home: NextPage = () => {
-  return <h1>Dashgo</h1>;
+const SignIn: NextPage = () => {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW="360"
+        bg="gray.800"
+        p="8"
+        borderRadius="8"
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input label="E-mail" name="email" type="email" />
+          <Input label="Password" name="password" type="password" />
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Sign in
+        </Button>
+      </Flex>
+    </Flex>
+  );
 };
 
-export default Home;
+export default SignIn;
