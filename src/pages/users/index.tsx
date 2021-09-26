30c02501ffa88da00b72954f5e81d4b2a74cbf1b
Fernando Goncalves <fernando.hmg@gmail.com>
2021-09-25 21:49:01 -0300

    Pagination and user page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,75 @@
+import { Button } from "@chakra-ui/button";
+import { Checkbox } from "@chakra-ui/checkbox";
+import Icon from "@chakra-ui/icon";
+import { Box, Flex, Heading } from "@chakra-ui/layout";
+import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
+import React from "react";
+import { RiAddFill, RiPencilFill } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Paginations";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxW="1480" mx="auto" px="6">
+        <Sidebar />
+        <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Users
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddFill} fontSize="20" />}
+            >
+              Create
+            </Button>
+          </Flex>
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>User</Th>
+                <Th>Creation date</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr px="6">
+                <Td>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box fontWeight="bold">Fernando Gonçalves</Box>
+                  <Box fontSize="sm" color="gray.300">
+                    fernando.hmg@gmail.com
+                  </Box>
+                </Td>
+                <Td>24 de setembro, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilFill} fontSize="16" />}
+                  >
+                    Edit
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
