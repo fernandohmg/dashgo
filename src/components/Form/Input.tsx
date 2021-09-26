26e44a2a2ef22dc301a93b9b0afc9ce3a85b5abd
Fernando Goncalves <fernando.hmg@gmail.com>
2021-09-26 13:32:53 -0300

    Adding react-hook-form

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,33 +1,41 @@
 import {
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input as ChakraInput,
   InputProps as ChakraInputProps,
 } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
-  return (
-    <FormControl>
-      {!!label && (
-        <FormLabel id={`${name}-label`} htmlFor={name}>
-          {label}
-        </FormLabel>
-      )}
-      <ChakraInput
-        id={name}
-        name={name}
-        focusBorderColor="pink.500"
-        bgColor="gray.900"
-        variant="filled"
-        _hover={{ bgColor: "gray.900" }}
-        size="lg"
-        {...rest}
-      />
-    </FormControl>
-  );
-}
+export const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> =
+  ({ name, label, error = null, ...rest }, ref) => {
+    return (
+      <FormControl isInvalid={!!error}>
+        {!!label && (
+          <FormLabel id={`${name}-label`} htmlFor={name}>
+            {label}
+          </FormLabel>
+        )}
+        <ChakraInput
+          id={name}
+          name={name}
+          focusBorderColor="pink.500"
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{ bgColor: "gray.900" }}
+          size="lg"
+          ref={ref}
+          {...rest}
+        />
+        {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+      </FormControl>
+    );
+  };
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,22 @@ import { Flex, Stack } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/Input";
 
+type SigninFormData = {
+  email: string;
+  password: string;
+};
+
 const SignIn: NextPage = () => {
+  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+
+  const handleSignin: SubmitHandler<SigninFormData> = (values) => {
+    console.log(values);
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -15,12 +28,29 @@ const SignIn: NextPage = () => {
         p="8"
         borderRadius="8"
         flexDir="column"
+        onSubmit={handleSubmit(handleSignin)}
       >
         <Stack spacing="4">
-          <Input label="E-mail" name="email" type="email" />
-          <Input label="Password" name="password" type="password" />
+          <Input
+            label="E-mail"
+            type="email"
+            error={errors.email}
+            {...register("email", { required: "E-mail is required" })}
+          />
+          <Input
+            label="Password"
+            type="password"
+            error={errors.password}
+            {...register("password", { required: "Password is required" })}
+          />
         </Stack>
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
           Sign in
         </Button>
       </Flex>
